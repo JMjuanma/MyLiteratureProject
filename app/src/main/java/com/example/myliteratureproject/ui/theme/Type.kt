298c9931013a98812ae3534ef10ce085ca1ad3d9
JMjuanma <juanma.soruco@gmail.com>
2022-09-26 21:45:02 -0400

    Why?
    Unespected errors

--- app/src/main/java/com/example/myliteratureproject/ui/theme/Type.kt
+++ app/src/main/java/com/example/myliteratureproject/ui/theme/Type.kt
@@ -1,17 +1,24 @@
 package com.example.myliteratureproject.ui.theme
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.myliteratureproject.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = R.font.varela_round,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+    ),
+    body1 = TextStyle(
+        fontFamily = R.font.aleo,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
     )
     /* Other default text styles to override
     button = TextStyle(
